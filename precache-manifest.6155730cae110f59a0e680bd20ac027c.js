6913e9206645edae016ea93abb00e0a724cdefca
Conner <cbluck3@gatech.edu>
2021-08-19 23:01:17 -0400

    Updates

--- precache-manifest.6155730cae110f59a0e680bd20ac027c.js
+++ precache-manifest.6155730cae110f59a0e680bd20ac027c.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9e9bcd0beead3ccf95ae4b08e42e9521",
+    "revision": "d9b051356d56f82d6308cd1e352f5de5",
     "url": "/home/index.html"
   },
   {
-    "revision": "17244bc618c2087d706e",
+    "revision": "160b83539d10cd23b9cc",
     "url": "/home/static/css/main.e16032d7.chunk.css"
   },
   {
-    "revision": "c6ca6c84ee2a02bfc5f5",
-    "url": "/home/static/js/2.75376d99.chunk.js"
+    "revision": "087ab6aafd03aef33f4d",
+    "url": "/home/static/js/2.04d92a9e.chunk.js"
   },
   {
     "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
-    "url": "/home/static/js/2.75376d99.chunk.js.LICENSE.txt"
+    "url": "/home/static/js/2.04d92a9e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "17244bc618c2087d706e",
-    "url": "/home/static/js/main.cbf173ea.chunk.js"
+    "revision": "160b83539d10cd23b9cc",
+    "url": "/home/static/js/main.7768321b.chunk.js"
   },
   {
     "revision": "c0578b487099793a068c",
     "url": "/home/static/js/runtime-main.3c590061.js"
   },
   {
-    "revision": "32f6b574eb11519020e24620a38bdf55",
-    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+    "revision": "ab756ae31f249683e3dfc7b7867bd1e1",
+    "url": "/home/static/media/me.ab756ae3.jpg"
   },
   {
     "revision": "9d3da0e22086c40d5e3106d01c0f6402",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.52c077f603228758c357b0a51a3c2bd6.js"
+  "/home/precache-manifest.6155730cae110f59a0e680bd20ac027c.js"
 );
 
 self.addEventListener('message', (event) => {
